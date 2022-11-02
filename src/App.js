072a9b42ecea16289282677916c40c542c3257c2
Dario Turin <dturin1997@gmail.com>
2022-11-02 11:57:39 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Este es mi primer componente</h1>
+      <h2>El valor de number es: {number}</h2>
+      <button onClick={() => setNumber(number + 1)}>Incrementar</button>
+      <button onClick={() => setNumber(number - 1)}>Decrementar</button>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2022 Tecsup - Alumnos inc</p>
+        </footer>
+    );
+};
+
+export default Footer;
